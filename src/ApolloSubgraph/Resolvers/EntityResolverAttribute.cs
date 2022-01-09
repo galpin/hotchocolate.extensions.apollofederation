b6587fb44923fb49bbe3a8092ba1f84407a7ef6c
Martin Galpin <galpin@gmail.com>
2022-01-09 17:22:20 +0000

    Update xmldoc.

--- src/ApolloSubgraph/Resolvers/EntityResolverAttribute.cs
+++ src/ApolloSubgraph/Resolvers/EntityResolverAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace HotChocolate.Extensions.ApolloSubgraph;
 
 /// <summary>
-/// Indicates that the annotated method is a resolver for a federated type.
+/// Indicates that the annotated method is a resolver for a federated entity.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class EntityResolverAttribute : Attribute
